94142152cdb8cd399e7878ab8e552eec97067713
xplodwild <xplod@ouverta.fr>
2021-12-06 22:12:38 +0100

    kaito: Use MediaTekRIL class

--- device.mk
+++ device.mk
@@ -2,3 +2,7 @@ DEVICE_PATH := device/bq/kaito
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/bq/kaito/kaito-vendor.mk)
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.ril_class=MediaTekRIL
